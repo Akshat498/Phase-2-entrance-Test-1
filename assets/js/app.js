ca1ce24f757e501a10c461902e4e3bcd69c4dcdd
akshat498 <akshatdhukia23@gmail.com>
2025-02-10 12:40:03 +0530

    hero-section-complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,31 @@ const navbarbtn = () => {
     document.getElementById("rotats2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
   }
+
+  $('.meet-our-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+
+
+    ]
+});
